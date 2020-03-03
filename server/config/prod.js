1b15aac8bad878951aff0cadd9c63280d87a5240
Quentin Vidal <qisforq@gmail.com>
2020-03-03 17:55:34 -0500

    removed unneeded comment from bottom of prod.js file

--- server/config/prod.js
+++ server/config/prod.js
@@ -5,4 +5,3 @@ module.exports = {
   herokuPostgresURI: process.env.DATABASE_URL,
   cookieKey: process.env.COOKIE_KEY
 }
-// If herokuPostgresURI isn't working, check to make sure it is still the same on heroku: https://data.heroku.com/datastores/da09fbdc-41dc-46db-954b-d3753614c58e#administration or by running "heroku config" on the cli inside the project folder (Make sure you look at the PRODUCTION database, which is DATABASE_URL)
