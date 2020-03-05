17407d8aab2d311070d8df1fe39d57b54520c640
Quentin Vidal <qisforq@gmail.com>
2020-03-05 17:14:16 -0500

    added App.js

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,29 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Users from './components/Users.jsx'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-
-  }
-
-
-
-  render() {
-      return (
-        <div >
-          <div className="jumbotron text-center" style={{backgroundImage : `linear-gradient(rgb(70,90,201), rgb(199,205,238))`, fontFamily: 'sans-serif'}}>
-          <h1 style={{color : "white"}}>eSend Ripple API Dashboard</h1>
-          </div>
-          <p>
-            <a href="/auth/google">Sign In With Google</a>
-          </p>
-        </div>
-      );
-    }
-}
 
 ReactDOM.render(<App />, document.getElementById('app'));
