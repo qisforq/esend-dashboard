cd63d4467777762aab926d3772ae8bcce9f0b1bc
Quentin Vidal <qisforq@gmail.com>
2020-03-05 13:36:57 -0500

    added esend logo ascii art to server start message and added link to front end to sign in with google

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,33 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ComponentTest from "./components/ComponentTest.jsx";
 import Users from './components/Users.jsx'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: null,
     };
-    this.getUsername = this.getUsername.bind(this); 
 
   }
 
-  getUsername(name) {
-    this.setState({ username: name }, () => {
-      console.log(`ping? you got a ${this.state.username} in there?`);
-      axios.post('/users', {
-        username: this.state.username,
-      })
-        .then((res) => {
-          console.log('ping!? here\'s the .then... (success?)');
-          // do something
-        })
-        .catch((err) => {
-          console.log(`error on post to /users: ${err}`);
-        });
-    });
-  }
 
 
   render() {
@@ -36,14 +18,9 @@ class App extends React.Component {
           <div className="jumbotron text-center" style={{backgroundImage : `linear-gradient(rgb(70,90,201), rgb(199,205,238))`, fontFamily: 'sans-serif'}}>
           <h1 style={{color : "white"}}>eSend Ripple API Dashboard</h1>
           </div>
-          <Users
-            handleName={this.handleName}
-            username={this.state.username}
-          />
-          {
-            this.state.username && // render after username is selected
-            <ComponentTest />
-          }
+          <p>
+            <a href="/auth/google">Sign In With Google</a>
+          </p>
         </div>
       );
     }
--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 const path = require('path');
 const cookieSession = require('cookie-session')
 const keys = require('./config/keys')
-
+const glyph = require('./extras')
 
 require('./services/passport');
 
@@ -25,4 +25,9 @@ require('./routes/authRoutes')(app)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}!`);
+  console.log(glyph.esendGlyph);
+  console.log(glyph.bannerMessage);
+  
+  
+  
 });
