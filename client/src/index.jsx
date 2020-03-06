a76d54ebf625ae0bd57c40d0ead093f9cc6a0d29
Quentin Vidal <qisforq@gmail.com>
2020-03-06 11:10:19 -0500

    initial integration of redux

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {  createStore, applyMiddleware } from 'redux';
 
+import App from './components/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.querySelector('#app')
+);
